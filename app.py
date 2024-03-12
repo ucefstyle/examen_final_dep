450e9aeaf59e049602a3f604a83675027ecac161
ucefsky <ucefouazzani@gmail.com>
2024-03-12 18:06:05 -0400

    ajout fichier python

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# app.py
+import sys
+
+def main():
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
+
+if __name__ == "__main__":
+    main()
+
